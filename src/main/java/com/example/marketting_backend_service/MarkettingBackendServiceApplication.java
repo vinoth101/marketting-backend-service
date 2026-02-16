f2ecb2b6242ae03f99541ad33dc28201f335f4d9
Seraladhan1023 <seraladhan2310@gmail.com>
2026-02-16 20:42:23 +0530

    Initial commit for marketting project

---
+++ src/main/java/com/example/marketting_backend_service/MarkettingBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketting_backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkettingBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkettingBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketting_backend_service/MarkettingBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketting_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkettingBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
